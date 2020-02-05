8519c447566bb2340e86fda1e4698a2ffe6e1a48
Jason Corpuz <jason.corpuz@live.com>
2020-02-05 10:31:53 -0800

    add cors

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ const morganOption = (NODE_ENV === 'production')
   : 'common';
 
 app.use(morgan(morganOption))
-app.use(cors({ origin: 'http://localhost:3000' }))
+app.use(cors())
 // app.use(function(req, res, next) {
 //   res.header("Access-Control-Allow-Origin", "*");
 //   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
