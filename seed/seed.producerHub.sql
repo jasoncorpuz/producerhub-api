faeeaa11d961c1ac74a66968267761519555683c
Jason Corpuz <jason.corpuz@live.com>
2020-02-05 08:02:19 -0800

    predeploy

--- seed/seed.producerHub.sql
+++ seed/seed.producerHub.sql
@@ -16,12 +16,12 @@ VALUES
 
 INSERT INTO songs (title, location, user_id, description)
 VALUES
-    ('Rach Rhapsody', 'somelocation.com', 1 , 'Vitae turpis massa sed elementum tempus egestas sed. Justo donec enim diam vulputate ut pharetra sit amet. Consectetur purus ut faucibus pulvinar elementum integer enim.'),
-    ('Under Shreksure', 'somelocation.com', 2 , 'Vitae turpis massa sed elementum tempus egestas sed. Justo donec enim diam vulputate ut pharetra sit amet. Consectetur purus ut faucibus pulvinar elementum integer enim.'),
-    ('Trappers Delight', 'somelocation.com', 3, 'Vitae turpis massa sed elementum tempus egestas sed. Justo donec enim diam vulputate ut pharetra sit amet. Consectetur purus ut faucibus pulvinar elementum integer enim.'),
-    ('New City Path', 'somelocation.com', 3 , 'Vitae turpis massa sed elementum tempus egestas sed. Justo donec enim diam vulputate ut pharetra sit amet. Consectetur purus ut faucibus pulvinar elementum integer enim.'),
-    ('The Campfire Song Song', 'somelocation.com', 4 , 'Vitae turpis massa sed elementum tempus egestas sed. Justo donec enim diam vulputate ut pharetra sit amet. Consectetur purus ut faucibus pulvinar elementum integer enim.'),
-    ('Elvis is Actually Bad', 'somelocation.com', 5 , 'Vitae turpis massa sed elementum tempus egestas sed. Justo donec enim diam vulputate ut pharetra sit amet. Consectetur purus ut faucibus pulvinar elementum integer enim.');
+    ('Rach Rhapsody', 'https://test-300.s3-us-west-1.amazonaws.com/KVCL_Ind_Perc_Ens_4_125_Loop_5-8_13.wav', 1 , 'Vitae turpis massa sed elementum tempus egestas sed. Justo donec enim diam vulputate ut pharetra sit amet. Consectetur purus ut faucibus pulvinar elementum integer enim.'),
+    ('Under Shreksure', 'https://test-300.s3-us-west-1.amazonaws.com/OS_LFS_115_Cmin_Worn_Chords_Extended.wav', 2 , 'Vitae turpis massa sed elementum tempus egestas sed. Justo donec enim diam vulputate ut pharetra sit amet. Consectetur purus ut faucibus pulvinar elementum integer enim.'),
+    ('Trappers Delight', 'https://test-300.s3-us-west-1.amazonaws.com/OS_MDNB_174_Fm_Popcorn.wav', 3, 'Vitae turpis massa sed elementum tempus egestas sed. Justo donec enim diam vulputate ut pharetra sit amet. Consectetur purus ut faucibus pulvinar elementum integer enim.'),
+    ('New City Path', 'https://test-300.s3-us-west-1.amazonaws.com/PG_115_Guitarloops_24_Am.wav', 3 , 'Vitae turpis massa sed elementum tempus egestas sed. Justo donec enim diam vulputate ut pharetra sit amet. Consectetur purus ut faucibus pulvinar elementum integer enim.'),
+    ('The Campfire Song Song', 'https://test-300.s3-us-west-1.amazonaws.com/PL_EHV2_05_C_Piano_125_C%23min_Tail.wav', 4 , 'Vitae turpis massa sed elementum tempus egestas sed. Justo donec enim diam vulputate ut pharetra sit amet. Consectetur purus ut faucibus pulvinar elementum integer enim.'),
+    ('Elvis is Actually Bad', 'https://test-300.s3-us-west-1.amazonaws.com/Perc_Loops_RadioPerc_120.wav', 5 , 'Vitae turpis massa sed elementum tempus egestas sed. Justo donec enim diam vulputate ut pharetra sit amet. Consectetur purus ut faucibus pulvinar elementum integer enim.');
 
 INSERT INTO likes (song_id, user_id)
 VALUES
