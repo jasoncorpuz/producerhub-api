071f56b34740cfa5aa44548bafe6f8ace91db7c5
Jason Corpuz <jason.corpuz@live.com>
2020-02-05 08:58:13 -0800

    still trying to fix cors

--- src/app.js
+++ src/app.js
@@ -19,7 +19,13 @@ const morganOption = (NODE_ENV === 'production')
   : 'common';
 
 app.use(morgan(morganOption))
-app.use(cors({ origin: '*' }))
+// app.use(cors({ origin: '*' }))
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(helmet())
 
 app.use(busboy())
